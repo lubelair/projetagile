b366ee19146788699eb8561ca84e670a90a4aabc
AmAlilou <alilou.amine2012@gmail.com>
2017-03-31 11:16:59 +0200

    commit check

---
+++ JoinMe/JoinMe/Controllers/AppController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+
+namespace JoinMe.Controllers
+{
+    public class AppController : ApiController
+    {
+        [HttpGet, HttpPost]
+        public IHttpActionResult Index()
+        {
+            return Ok("toto");
+        }
+    }
+}
