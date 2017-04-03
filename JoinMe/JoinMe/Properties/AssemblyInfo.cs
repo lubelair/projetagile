a2b0629b91bc42762d1078b822620ec8b7117cbc
AmAlilou <alilou.amine2012@gmail.com>
2017-04-03 15:23:59 +0200

    update assembly

--- JoinMe/JoinMe/Properties/AssemblyInfo.cs
+++ JoinMe/JoinMe/Properties/AssemblyInfo.cs
@@ -2,21 +2,20 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Les informations générales relatives à un assembly dépendent de 
-// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
-// associées à un assembly.
-[assembly: AssemblyTitle("JoinMe")]
+// Les informations générales relatives à un assembly dépendent de l'ensemble d'attributs suivant.
+// Changez les valeurs de ces attributs pour modifier les informations associées à un assembly.
+[assembly: AssemblyTitle("JoinMeServices")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JoinMe")]
+[assembly: AssemblyProduct("JoinMeServices")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly 
-// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de 
-// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly aux
+// composants COM. Si vous devez accéder à un type dans cet assembly à partir de COM, affectez la
+// valeur true à l'attribut ComVisible sur ce type.
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
@@ -24,12 +23,9 @@ using System.Runtime.InteropServices;
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
-//      Version principale
-//      Version secondaire 
-//      Numéro de build
-//      Révision
+// Version principale Version secondaire Numéro de build Révision
 //
-// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par défaut 
-// en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de révision et de build par
+// défaut en utilisant '*', comme indiqué ci-dessous :
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
