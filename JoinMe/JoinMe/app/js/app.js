7d4d07c4811a9e63de6387c08b8b77532972d33a
ZaydBenGara <bengarazayd@gmail.com>
2017-04-04 10:46:55 +0200

    mise a jour de la mort

--- JoinMe/JoinMe/app/js/app.js
+++ JoinMe/JoinMe/app/js/app.js
@@ -1,27 +1,40 @@
+// Ionic Starter App
+
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'directory' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'directory.services' is found in services.js
-// 'directory.controllers' is found in controllers.js
-angular.module('directory', ['ionic', 'directory.services', 'directory.controllers'])
+angular.module('starter', ['ionic'])
 
-    .config(function ($stateProvider, $urlRouterProvider) {
-        // Ionic uses AngularUI Router which uses the concept of states
-        // Learn more here: https://github.com/angular-ui/ui-router
-        // Set up the various states which the app can be in.
-        // Each state's controller can be found in controllers.js
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-        $stateProvider.state('connexion', {
-            url: '/login',
-            templateUrl: 'templates/connexion.html',
-            controller: 'connexionCtrl'
-        })
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
 
-        $stateProvider.state('inscription', {
-            url: '/subscribe',
-            templateUrl: 'templates/inscription.html',
-            controller: 'inscriptionCtrl'
-        })
+.config (function($stateProvider, $urlRouterProvider){
 
-        $urlRouterProvider.otherwise('/login')
-    });
+  $stateProvider.state('inscription',{
+    url:'/inscription',
+    templateUrl:'templates/inscription.html'
+  })
+
+  $stateProvider.state('authentification',{
+    url:'/authentification',
+    templateUrl:'templates/authentification.html'
+  })
+
+  $urlRouterProvider.otherwise('/authentification')
+
+})
