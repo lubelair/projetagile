519f14ccd86f570a2c11d7debb0d17fc7079b84d
AmAlilou <alilou.amine2012@gmail.com>
2017-04-04 17:34:04 +0200

    Index .html

--- JoinMe/JoinMe/app/js/app.js
+++ JoinMe/JoinMe/app/js/app.js
@@ -3,38 +3,36 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('directory', ['ionic', 'directory.services', 'directory.controllers'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+            // Don't remove this line unless you know what you are doing. It stops the viewport
+            // from snapping when text inputs are focused. Ionic handles this internally for
+            // a much nicer keyboard experience.
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
 })
 
-.config (function($stateProvider, $urlRouterProvider){
+.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('inscription', {
+        url: '/inscription',
+        templateUrl: 'templates/inscription.html'
+    })
 
-  $stateProvider.state('inscription',{
-    url:'/inscription',
-    templateUrl:'templates/inscription.html'
-  })
+    $stateProvider.state('authentification', {
+        url: '/authentification',
+        templateUrl: 'templates/authentification.html'
+    })
 
-  $stateProvider.state('authentification',{
-    url:'/authentification',
-    templateUrl:'templates/authentification.html'
-  })
-
-  $urlRouterProvider.otherwise('/authentification')
-
+    $urlRouterProvider.otherwise('/authentification')
 })
