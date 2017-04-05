b417fd9c7d55b58b932eed8a380e1b5a0a40bcc6
AmAlilou <alilou.amine2012@gmail.com>
2017-04-05 11:37:24 +0200

    Changing state

--- JoinMe/JoinMe/app/js/controllers.js
+++ JoinMe/JoinMe/app/js/controllers.js
@@ -1,15 +1,22 @@
 angular.module('directory.controllers', [])
 
 //AppService => name of service in service.js
-    .controller('SubscribeCtrl', function ($scope, AppService) {
+    .controller('SubscribeCtrl', function ($scope, $state, AppService) {
         $scope.createUser = function (user) {
             // call createUser method of appService
             //   AppService.createUser(user);
             alert("toto");
         }
     })
-    .controller('AuthentificationCtrl', function ($scope, AppService) {
+    .controller('AuthentificationCtrl', function ($scope, $state, AppService) {
         $scope.connect = function (login, password) {
             // AppService.connect(_Login, _Password);
+        }
+        $scope.inscription = function () {
+            //change state to inscription state 
+            $state.go('inscription');
+        }
+        $scope.forgetPassword = function () {
+            //$state.go('forgetPassword');
         }
     });
