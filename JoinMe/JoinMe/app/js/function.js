4ef0b57192687e0a0b84207dd9f2cf744f08d67a
lubelair <lulucant@hotmail.fr>
2017-04-06 16:01:09 +0200

    Ajout affichage dynamique checkbox

--- JoinMe/JoinMe/app/js/function.js
+++ JoinMe/JoinMe/app/js/function.js
@@ -1,5 +1,10 @@
 function checkPwd() {
-    if(document.getElementById("passwordValid").value != document.getElementById("password").value)
+    if (document.getElementById("passwordValid").value != document.getElementById("password").value) {
+        document.getElementById("false").style.display = 'block';
+        document.getElementById("valid").style.display = 'none';
         return false;
+    }
+    document.getElementById("false").style.display = 'none';
+    document.getElementById("valid").style.display = 'block';
     return true;
 }
