8cb38141e6ce2d2e16c85e70413599cbf521de36
lubelair <lulucant@hotmail.fr>
2017-04-07 13:49:04 +0200

    Retrait tests

--- JoinMe/JoinMe/app/js/controllers.js
+++ JoinMe/JoinMe/app/js/controllers.js
@@ -9,6 +9,7 @@ angular.module('directory.controllers', [])
 
         $scope.createUser = function (user) {
             AppService.createUser(user);
+            state.go('accueil')
         }
 
         $scope.authentification = function () {
