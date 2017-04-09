ed4ba6fd329b3d8f41ea881beec1c0c8a7648617
AmAlilou <alilou.amine2012@gmail.com>
2017-04-09 03:19:10 +0200

    Rename AppController.cs to ServiceController.cs

---
+++ JoinMe/JoinMe/Controllers/ServiceController.cs
@@ -0,0 +1,42 @@
+﻿using JoinMeServices.Models;
+using System;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace JoinMe.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        #region Private Fields
+
+        private JoinMeServicesContext db = new JoinMeServicesContext();
+
+        #endregion Private Fields
+
+        #region Public Methods
+
+        [HttpGet, HttpPost]
+        public IHttpActionResult Index()
+        {
+            return Ok("joinMe web api");
+        }
+
+        [ResponseType(typeof(User))]
+        [HttpGet, HttpPost]
+        public async Task<Object> PostUser(User user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Users.Add(user);
+            await db.SaveChangesAsync();
+
+            return user;
+        }
+
+        #endregion Public Methods
+    }
+}
