9b970c0bf57294963c16304502a0e34e705c28a2
AmAlilou <alilou.amine2012@gmail.com>
2017-04-11 18:49:16 +0200

    Add directives file and friend page

--- JoinMe/JoinMe/app/js/directives.js
+++ JoinMe/JoinMe/app/js/directives.js
@@ -1,1 +1,22 @@
 ﻿angular.module('directory.directives', [])
+  .directive('userSpace', [function () {
+      var dtr = new Date().getTime();
+      return {
+          restrict: 'A',
+          replace: true,
+          scope: true,
+          templateUrl: 'templates/UserSpace.html?' + dtr,
+          link: function (scope, element, attrs) {
+          }
+      };
+  }])
+ .directive('calendarView', [function () {
+     var dtr = new Date().getTime();
+     return {
+         restrict: 'A',
+         replace: true,
+         scope: true,
+         templateUrl: 'templates/Calendar.html?' + dtr,
+         link: function (scope, element, attrs) { }
+     }
+ }])
