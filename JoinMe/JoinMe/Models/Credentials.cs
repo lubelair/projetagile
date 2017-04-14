26e89b0c7103ab468855742cc0c2abebb5319112
lubelair <lulucant@hotmail.fr>
2017-04-14 10:04:53 +0200

    Ajout classe Credentials

---
+++ JoinMe/JoinMe/Models/Credentials.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JoinMeServices.Models
+{
+    public class Credentials
+    {
+        #region Public Properties
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        #endregion Public Properties
+    }
+}
